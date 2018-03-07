30f0a5d2896b8a58b71b3fd113d7a51f9b03d562
chengdaqian <chengdaqian@buaa.edu.cn>
2018-03-07 20:53:24 +0800

    try

--- src/pointcloud2_filter.cpp
+++ src/pointcloud2_filter.cpp
@@ -1,6 +1,8 @@
 #include <ros/ros.h>
 // PCL specific includes
 #include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/point_cloud_conversion.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -8,6 +10,7 @@
 
 ros::Publisher pub;
 double filter_resolution_;
+double uav_fov_;
 
 void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
@@ -30,6 +33,19 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
   sensor_msgs::PointCloud2 output;
   pcl_conversions::fromPCL(cloud_filtered, output);
 
+    sensor_msgs::PointCloud point_cloud_1;
+    sensor_msgs::convertPointCloud2ToPointCloud(output, point_cloud_1);
+
+    float y, z;
+    for (auto it = point_cloud_1.points.begin(); it != point_cloud_1.points.end(); it++){
+        y = it->y;
+        z = it->z;
+        if (y > (uav_fov_/2 + 0.07) || y < (-uav_fov_/2 + 0.07) || abs(z) > uav_fov_/2)
+            point_cloud_1.points.erase(it);
+    }
+    sensor_msgs::convertPointCloudToPointCloud2(point_cloud_1, output);
+
+
   // Publish the data
   pub.publish (output);
 }
@@ -38,12 +54,13 @@ int main (int argc, char** argv)
 {
   // Initialize ROS
   ros::init (argc, argv, "pointcloud2_filter");
-  ros::NodeHandle nh;
+  ros::NodeHandle nh("~");
 
   // Create a ROS subscriber for the input point cloud
   ros::Subscriber sub = nh.subscribe ("input_cloud", 1, cloud_cb);
 
   nh.param<double>("filter_resolution", filter_resolution_, 0.1);
+    nh.param<double>("uav_fov", uav_fov_, 2.0);
 
   // Create a ROS publisher for the output point cloud
   pub = nh.advertise<sensor_msgs::PointCloud2> ("output_cloud", 1);
